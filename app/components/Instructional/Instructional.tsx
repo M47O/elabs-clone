65e23ad5a137b2f6331581f927238b1aec862836
Mason Turner <turner_m@lynchburg.edu>
2023-11-22 13:29:29 -0500

    Add Instructional & PromptContainer components

---
+++ app/components/Instructional/Instructional.tsx
@@ -0,0 +1,10 @@
+export default function Instructional() {
+	return (
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+			obcaecati a beatae incidunt eaque id explicabo natus velit debitis rem
+			blanditiis veniam, dolorem consequatur numquam perferendis. Accusamus
+			impedit maxime fugiat.
+		</p>
+	)
+}
---
+++ app/components/PromptContainer/PromptContainer.tsx
@@ -0,0 +1,12 @@
+export default function PromptContainer() {
+	return (
+		<form>
+			<select>
+				<option value="">Option 1</option>
+				<option value="">Option 2</option>
+			</select>
+			<textarea></textarea>
+			<button>Submit</button>
+		</form>
+	)
+}
